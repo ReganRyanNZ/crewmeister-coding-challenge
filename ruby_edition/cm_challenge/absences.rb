0b44b58210e24200add8e05ea2b674b90fd41fbf
Regan Ryan <regan.ryan.nz@gmail.com>
2019-04-30 22:47:31 +0200

    added Absences#to_ical to create ical from absence data, with tests

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -1,9 +1,55 @@
 require_relative './api'
+require 'icalendar'
 
 module CmChallenge
   class Absences
     def to_ical
-      # Your implementation here
+      @absences = CmChallenge::Api.absences
+      @members = CmChallenge::Api.members
+
+      cal = Icalendar::Calendar.new
+      @absences.each do |absence|
+        member = @members.find { |member| member[:user_id] == absence[:user_id] }
+        cal.event do |e|
+          e.dtstart     = Icalendar::Values::Date.new(absence[:start_date].gsub('-', ''))
+          e.dtend       = Icalendar::Values::Date.new(absence[:end_date].gsub('-', ''))
+          e.summary     = absence_summary(absence[:type], member[:name])
+          e.description = absence_description(absence[:member_note], member[:name])
+          e.ip_class    = "PRIVATE"
+        end
+      end
+      cal.publish
+      cal
+    end
+
+    private
+
+    def absence_description member_note, name
+      member_note.empty? ? "" : "#{name}'s note: #{member_note}"
+    end
+
+    def absence_summary type, name
+      case type
+      when "vacation"
+        "#{name} is on vacation"
+      when "sickness"
+        "#{name} is sick"
+      else
+        "#{name} is absent"
+      end
     end
   end
 end
+
+# {:admitter_id=>nil
+#   :admitter_note=>""
+#   :confirmed_at=>"2017-04-11T08:24:06.000+02:00"
+#   :created_at=>"2017-04-09T21:17:12.000+02:00"
+#   :crew_id=>352
+#   :end_date=>"2017-04-26"
+#   :id=>4471
+#   :member_note=>"bitte den urlaub in der vorwoche  17-21 zurückziehen. merci :)"
+#   :rejected_at=>nil
+#   :start_date=>"2017-04-24"
+#   :type=>"vacation"
+#   :user_id=>2796}
--- ruby_edition/spec/absences_spec.rb
+++ ruby_edition/spec/absences_spec.rb
@@ -1,5 +1,28 @@
 require_relative '../cm_challenge/absences'
+require 'icalendar'
 
 RSpec.describe 'Absences' do
-  it { skip 'Your tests here' }
+  describe 'to_ical' do
+    subject { CmChallenge::Absences.new.to_ical }
+    it 'returns an ical object' do
+      expect(subject.class).to eq(Icalendar::Calendar)
+    end
+
+    it 'includes all events in ical object' do
+      expect(subject.events.count).to eq(42)
+    end
+
+    it 'includes correct data in events' do
+      expect(subject.events.first.summary).to eq("Mike is sick")
+      expect(subject.events.first.description).to eq("")
+      expect(subject.events.first.dtstart.to_s).to eq("2017-01-13")
+      expect(subject.events.first.dtend.to_s).to eq("2017-01-13")
+
+      expect(subject.events.last.summary).to eq("Manuel is on vacation")
+      expect(subject.events.last.description).to eq("Manuel's note: Pfadfindersommerlager")
+      expect(subject.events.last.dtstart.to_s).to eq("2017-08-05")
+      expect(subject.events.last.dtend.to_s).to eq("2017-08-12")
+    end
+  end
+
 end
