2b4fa571acfa8dac443b02dbff6ab48bd211e456
Regan Ryan <regan.ryan.nz@gmail.com>
2019-04-30 23:10:28 +0200

    removed commented code

--- ruby_edition/cm_challenge/absences.rb
+++ ruby_edition/cm_challenge/absences.rb
@@ -40,16 +40,3 @@ module CmChallenge
     end
   end
 end
-
-# {:admitter_id=>nil
-#   :admitter_note=>""
-#   :confirmed_at=>"2017-04-11T08:24:06.000+02:00"
-#   :created_at=>"2017-04-09T21:17:12.000+02:00"
-#   :crew_id=>352
-#   :end_date=>"2017-04-26"
-#   :id=>4471
-#   :member_note=>"bitte den urlaub in der vorwoche  17-21 zurückziehen. merci :)"
-#   :rejected_at=>nil
-#   :start_date=>"2017-04-24"
-#   :type=>"vacation"
-#   :user_id=>2796}
